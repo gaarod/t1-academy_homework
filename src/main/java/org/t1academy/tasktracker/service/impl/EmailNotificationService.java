2987fd227534055a68367b6e7b9a18324a419de7
Andrey_Galan <galan.andrey@gmail.com>
2025-04-16 14:48:43 +0300

    homework 3

--- src/main/java/org/t1academy/tasktracker/service/impl/EmailNotificationService.java
+++ src/main/java/org/t1academy/tasktracker/service/impl/EmailNotificationService.java
@@ -7,7 +7,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.t1academy.tasktracker.aspect.annotation.LogException;
+import org.t1academy.logstarter.aspect.annotation.LogException;
 import org.t1academy.tasktracker.dto.TaskNotificationDto;
 import org.t1academy.tasktracker.service.NotificationService;
 
